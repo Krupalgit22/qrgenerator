53620d2950e5a3e429fe4bdc512f5a684a5884f5
Krupalgit22 <krupalatshivraecom@gmail.com>
2026-06-16 15:04:57 +0530

    Fix Render port

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const path = require("path");
 const QRCode = require("qrcode");
 
 const app = express();
-const PORT = 3000;
-
+// const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 
